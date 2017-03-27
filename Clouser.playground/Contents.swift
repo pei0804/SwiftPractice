795f2667bcb5923da0efa73a4c3d17ad28b911df
tikasan <satak47cpc@gmail.com>
2017-03-27 16:46:06 +0900

    update: closer

--- Clouser.playground/Contents.swift
+++ Clouser.playground/Contents.swift
@@ -43,10 +43,24 @@ func test2(atai1: Int, atai2: Int,isBig: (Int, Int) -> Bool) -> Bool {
 test2(atai1: 1, atai2: 2, isBig: isBig)
 
 //----------map----------
+// http://qiita.com/FuruyamaTakeshi/items/6f7c271a28720cb1d096
 // http://qiita.com/merrill/items/b3a57acf38afdd3023f0
 // http://swift.tecc0.com/?p=249
 // 各配列内の各要素をループで取り出して、処理をする
-// map { (a: "配列要素の型") -> "処理内容の返り値の型" in "処理内容" }
+
+// 普通に書くとこうなる。
+func multiplication(x: Int) -> Int {
+    return x * 2
+}
+var notUseMapArray = [1, 2, 3, 4, 5]
+var notUseMapArrayResult = [Int]()
+
+for n in notUseMapArray {
+    notUseMapArrayResult.append(multiplication(x: n))
+}
+print(notUseMapArrayResult)
+
+
 let arrayMap = [1, 2, 3, 4, 5].map{(a: Int) -> Int in
     a * 2
 }
@@ -54,14 +68,34 @@ let arrayMap = [1, 2, 3, 4, 5].map{(a: Int) -> Int in
 let arrayMap2 = [1, 2, 3, 4, 5].map{ $0 * 2}
 
 //----------filter----------
+
+// 普通に書くとこうなる
+var notUseFilterArray = [1, 2, 3, 4 ,5]
+var notUseFilterArrayResult = [Int]()
+
+for n in notUseFilterArray {
+    if n > 2 {
+        notUseFilterArrayResult.append(n)
+    }
+}
+print(notUseFilterArrayResult)
+
 let arrayFilter = [1, 2, 3, 4, 5].filter{(a: Int) -> Bool in
-    return a > 1
+    return a > 2
 }
 
 let arrayFilter2 = [1, 2, 3, 4, 5].filter{$0 > 2}
 print(arrayFilter2)
 
 //----------reduce----------
+let notUseReduceArray = [1, 2, 3, 4, 5]
+
+var notUseReduceResult = 0
+for n in notUseReduceArray {
+    notUseReduceResult += n
+}
+print(notUseReduceResult)
+
 let arrayReduce = [1, 2, 3, 4, 5].reduce(0){(ans, num) -> Int in
     ans + num
 }
